dec9af5f607f4a3b9982ea69b16ae92c142448f7
Leonie113 <57359744+Leonie113@users.noreply.github.com>
2020-03-23 17:39:16 +0100

    JS Counter in function

--- assets/js/script_Leonie.js
+++ assets/js/script_Leonie.js
@@ -1,8 +1,27 @@
-function counter_function() {
-    var counter = document.getElementById("cntr");
-    var count = 0;
-    count = parseInt (counter.innerHTML);
-    count = count + 1;
-    counter.innerHTML = count;
-}
-window.onload = counter_function;
+const counters = document.querySelectorAll('.counter');
+const speed = 200; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = count + inc;
+			// Call function every ms
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
