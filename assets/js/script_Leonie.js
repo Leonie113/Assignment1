9106cd8cd9f619dd1d333d4709f17108b670f8bb
Leonie113 <57359744+Leonie113@users.noreply.github.com>
2020-03-25 14:23:45 +0100

    JS Kommentare

--- assets/js/script_Leonie.js
+++ assets/js/script_Leonie.js
@@ -1,22 +1,20 @@
+// Youtube und Instagram Counter
+
 const counters = document.querySelectorAll('.counter');
-const speed = 200; // The lower the slower
+const speed = 200; 
 
 counters.forEach(counter => {
 	const updateCount = () => {
 		const target = +counter.getAttribute('data-target');
 		const count = +counter.innerText;
 
-		// Lower inc to slow and higher to slow
+		
 		const inc = target / speed;
 
-		// console.log(inc);
-		// console.log(count);
-
-		// Check if target is reached
 		if (count < target) {
-			// Add inc to count and output in counter
+
 			counter.innerText = count + inc;
-			// Call function every ms
+
 			setTimeout(updateCount, 1);
 		} else {
 			counter.innerText = target;
@@ -26,6 +24,7 @@ counters.forEach(counter => {
 	updateCount();
 });
 
+// Scroll Event: Text wird beim Scrollen erscheinen
 function textAppear(){
 	var appearedText = document.querySelector('.appeared-text');
 	var introPosition = appearedText.getBoundingClientRect().top;
@@ -38,6 +37,8 @@ function textAppear(){
 
 window.addEventListener('scroll', textAppear);
 
+
+// Uhranzeige auf der Startseite
 setInterval(setClock, 1000)
 
 const hourPointer = document.querySelector('[data-hour-pointer]')
