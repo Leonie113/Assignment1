454a17b4045a778800301d997ab31dfee4707cde
Leonie113 <57359744+Leonie113@users.noreply.github.com>
2020-03-28 14:39:43 +0100

    AJAX Javascript

--- assets/js/script_Leonie.js
+++ assets/js/script_Leonie.js
@@ -104,3 +104,16 @@ for (i = 0; i <= dataReload.length; i++) {
 	}
 }
 
+
+//Ajax-Request (best travel destinations)
+
+function loadDoc() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+		  document.getElementById("Ajax").innerHTML = this.responseText;
+		}
+	  };
+	  xhttp.open("GET", "ajax_info.txt", true);
+	  xhttp.send();
+	}
